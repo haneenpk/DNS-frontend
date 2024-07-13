a229c8656b2c2fd3d15fa206b9e6ee25ab9f6979
haneenpk <mohdhaneenpk666@gmail.com>
2024-07-13 15:28:28 +0530

    category button changed

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 function Category() {
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 800);
+    const [selectedCategory, setSelectedCategory] = useState('DRINKS');
 
     useEffect(() => {
         const handleResize = () => {
@@ -16,18 +17,42 @@ function Category() {
         };
     }, []);
 
+    const handleButtonClick = (category) => {
+        setSelectedCategory(category);
+    };
+
+    const getClassName = (category) => {
+        return selectedCategory === category
+            ? 'bg-[#128be2] w-40 py-2 border-[#128be2] border-2'
+            : 'bg-black w-40 py-2 border-[#128be2] border-2';
+    };
+
     const backgroundImage = isMobile ? `url('/categoryPhone.png')` : `url('/categoryLap.png')`;
 
     return (
         <div>
-
             {isMobile ? (
                 <div style={{ backgroundImage, backgroundSize: 'cover', backgroundPosition: 'center', height: "95px" }}>
                     <div className='flex justify-center items-center text-white' style={{ fontSize: "12px" }}>
                         <div className='mt-6 space-x-4'>
-                            <button className='bg-black w-20 py-2 border-[#128be2] border-2'>FOOD</button>
-                            <button className='bg-[#128be2] w-20 py-2 border-[#128be2] border-2'>DRINKS</button>
-                            <button className='bg-black w-20 py-2 border-[#128be2] border-2'>BRUNCH</button>
+                            <button
+                                className={selectedCategory === 'FOOD' ? 'bg-[#128be2] w-20 py-2 border-[#128be2] border-2' : 'bg-black w-20 py-2 border-[#128be2] border-2'}
+                                onClick={() => handleButtonClick('FOOD')}
+                            >
+                                FOOD
+                            </button>
+                            <button
+                                className={selectedCategory === 'DRINKS' ? 'bg-[#128be2] w-20 py-2 border-[#128be2] border-2' : 'bg-black w-20 py-2 border-[#128be2] border-2'}
+                                onClick={() => handleButtonClick('DRINKS')}
+                            >
+                                DRINKS
+                            </button>
+                            <button
+                                className={selectedCategory === 'BRUNCH' ? 'bg-[#128be2] w-20 py-2 border-[#128be2] border-2' : 'bg-black w-20 py-2 border-[#128be2] border-2'}
+                                onClick={() => handleButtonClick('BRUNCH')}
+                            >
+                                BRUNCH
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -35,15 +60,30 @@ function Category() {
                 <div style={{ backgroundImage, backgroundSize: 'cover', backgroundPosition: 'center', height: "110px" }}>
                     <div className='flex justify-center items-center text-white' style={{ fontSize: "27px" }}>
                         <div className='mt-6 space-x-6'>
-                            <button className='bg-black w-40 py-2 border-[#128be2] border-2'>FOOD</button>
-                            <button className='bg-[#128be2] w-40 py-2 border-[#128be2] border-2'>DRINKS</button>
-                            <button className='bg-black w-40 py-2 border-[#128be2] border-2'>BRUNCH</button>
+                            <button
+                                className={getClassName('FOOD')}
+                                onClick={() => handleButtonClick('FOOD')}
+                            >
+                                FOOD
+                            </button>
+                            <button
+                                className={getClassName('DRINKS')}
+                                onClick={() => handleButtonClick('DRINKS')}
+                            >
+                                DRINKS
+                            </button>
+                            <button
+                                className={getClassName('BRUNCH')}
+                                onClick={() => handleButtonClick('BRUNCH')}
+                            >
+                                BRUNCH
+                            </button>
                         </div>
                     </div>
                 </div>
             )}
         </div>
-    )
+    );
 }
 
-export default Category
+export default Category;
